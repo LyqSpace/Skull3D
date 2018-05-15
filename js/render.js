c7bd48c4a9d57b1182fcbf678cca9d59be6c6335
LyqSerenity <root@lyq.me>
2018-05-15 23:45:27 +0800

    Add sticks

--- js/render.js
+++ js/render.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+    if (!Detector.webgl) Detector.addGetWebGLMessage();
 
     var camera, controls, scene, renderer, stats;
 
@@ -14,55 +14,57 @@ $(document).ready(function() {
 
         var canvas = $("#scene")[0];
 
-        renderer = new THREE.WebGLRenderer( {antialias: true} );
-        renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
-        renderer.setClearColor( 0x111111);
-        canvas.appendChild( renderer.domElement );
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+        renderer.setClearColor(0x111111);
+        canvas.appendChild(renderer.domElement);
 
         // camera
-        camera = new THREE.PerspectiveCamera( 45, canvas.offsetWidth/canvas.offsetHeight, 0.01, 4000 );
+        camera = new THREE.PerspectiveCamera(45, canvas.offsetWidth / canvas.offsetHeight, 0.01, 4000);
         camera.position.x = scale * 3;
 
         scene = new THREE.Scene();
 
         // helper
-        axes = new THREE.AxesHelper( scale * 2 )
-        scene.add( axes );
+        axes = new THREE.AxesHelper(scale * 2)
+        scene.add(axes);
 
         // light
-        var ambientLight = new THREE.AmbientLight( 0xffffff, 0.3 );
-        scene.add( ambientLight );
-        
+        var ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+        scene.add(ambientLight);
+
         var spotLight0 = new THREE.SpotLight(0xffffff, 1);
-        spotLight0.position.set( scale * 2, scale * 2, scale * 2 );
+        spotLight0.position.set(scale * 2, scale * 2, scale * 2);
         spotLight0.angle = Math.PI / 4;
-        scene.add( spotLight0 );
+        scene.add(spotLight0);
 
         var spotLight1 = new THREE.SpotLight(0xffffff, 1);
-        spotLight1.position.set( -scale * 2, scale * 2, -scale * 2 );
+        spotLight1.position.set(-scale * 2, scale * 2, -scale * 2);
         spotLight1.angle = Math.PI / 4;
-        scene.add( spotLight1 );
-        
+        scene.add(spotLight1);
+
         // light helper
-        lightHelper0 = new THREE.SpotLightHelper( spotLight0 );
-        scene.add( lightHelper0 );
-        
-        lightHelper1 = new THREE.SpotLightHelper( spotLight1 );
-		scene.add( lightHelper1 );
+        // lightHelper0 = new THREE.SpotLightHelper( spotLight0 );
+        // scene.add( lightHelper0 );
+
+        // lightHelper1 = new THREE.SpotLightHelper( spotLight1 );
+        // scene.add( lightHelper1 );
 
         // skull
         var skull = new Skull(scale);
         skull.init(fileName, function () {
-            
+
             scene.add(skull.bodyMesh);
-            // scene.add(skull.sticksMesh);
-            // console.log(skull.sticksMesh);
+            for (var i = 0; i < skull.sticksMesh.length; i++) {
+                scene.add(skull.sticksMesh[i]);
+            }
+            console.log(skull.sticks);
             console.log("Add skull");
 
         });
 
         // controls
-        controls = new THREE.OrbitControls( camera, renderer.domElement );
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
         controls.enableDamping = true;
         controls.dampingFactor = 0.25;
         controls.maxDistance = scale * 8;
@@ -70,7 +72,7 @@ $(document).ready(function() {
         // stats
         stats = new Stats();
         stats.domElement.style.zIndex = 100;
-        $("#control-sticks")[0].appendChild( stats.domElement );
+        $(".side")[0].insertBefore(stats.domElement, $("#control-sticks")[0]);
 
     }
 
@@ -79,18 +81,18 @@ $(document).ready(function() {
         camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
         camera.updateProjectionMatrix();
 
-        renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
+        renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
     }
 
     function animate() {
 
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
 
         controls.update();
         stats.update();
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
 
     }
-    
+
 });
--- js/skull.js
+++ js/skull.js
@@ -1,31 +1,81 @@
-var Skull = function(scale) {
+function getRotationAxis(vec) {
+
+    //Three.js uses a Y up coordinate system, so the cube inits with this vector
+    var upAxis = new THREE.Vector3(0, 1, 0);
+
+    //cross product of the up vector and direction vector
+    var axis = new THREE.Vector3();
+    axis.crossVectors(upAxis, vec);
+    axis.normalize();
+
+    return axis;
+
+}
+
+function getRotationAngle(vec) {
+
+    //Three.js uses a Y up coordinate system, so the cube inits with this vector
+    var upAxis = new THREE.Vector3(0, 1, 0);
+
+    //euler angle between direction vector and up vector
+    var angle = Math.acos(upAxis.dot(vec));
+
+    return angle;
+
+}
+
+function getStickMesh(stick, scale, color=0x00aabb) {
+
+    var geometry = new THREE.CylinderGeometry(1, 1, stick.len, 16);
+
+    var material = new THREE.MeshPhongMaterial();
+    material.side = THREE.DoubleSide;
+    material.color = new THREE.Color(color);
+
+    var halfLen = stick.len / 2;
+    var centerPt = new THREE.Vector3(
+        stick.startPt.x + halfLen * stick.vec.x,
+        stick.startPt.y + halfLen * stick.vec.y + scale,
+        stick.startPt.z + halfLen * stick.vec.z,
+    );
+
+    var cylinderMesh = new THREE.Mesh(geometry, material);
+    cylinderMesh.setRotationFromAxisAngle(stick.axis, stick.angle);
+    cylinderMesh.position.set(centerPt.x, centerPt.y, centerPt.z);
+    console.log(cylinderMesh.position);
     
+    return cylinderMesh;
+
+}
+
+var Skull = function (scale) {
+
     var object = this;
 
     object.scale = scale;
     object.bodyState = 0;
     object.sticksState = 0;
 
-    object.loadData = function(fileName, callback) {
-        
+    object.loadData = function (fileName, callback) {
+
         var xhttp = {};
         var bodyFilePath = "data/" + fileName + ".obj";
         var sticksFilePath = "data/" + fileName + "_sticks.cm";
 
-        loadXMLDoc( bodyFilePath, "body", function () {
-            
-            if ( xhttp["body"].readyState != 4 ) return;
-            
+        loadXMLDoc(bodyFilePath, "body", function () {
+
+            if (xhttp["body"].readyState != 4) return;
+
             object.bodyTxt = xhttp["body"].response;
             object.bodyState = 1;
             callback();
 
         });
 
-        loadXMLDoc( sticksFilePath, "sticks", function () {
-            
-            if ( xhttp["sticks"].readyState != 4 ) return;
-            
+        loadXMLDoc(sticksFilePath, "sticks", function () {
+
+            if (xhttp["sticks"].readyState != 4) return;
+
             object.sticksTxt = xhttp["sticks"].response;
             object.sticksState = 1;
             callback();
@@ -35,15 +85,15 @@ var Skull = function(scale) {
         function loadXMLDoc(filePath, name, callback) {
 
             if (window.XMLHttpRequest) {
-                
+
                 xhttp[name] = new XMLHttpRequest();
-        
+
             } else {// code for IE5 and IE6
-                
+
                 xhttp[name] = new ActiveXObject("Microsoft.XMLHTTP");
-        
+
             }
-        
+
             xhttp[name].onreadystatechange = callback;
             xhttp[name].overrideMimeType("text/plain; charset=utf-8");
             xhttp[name].open("GET", filePath, true);
@@ -53,10 +103,10 @@ var Skull = function(scale) {
 
     };
 
-    object.init = function(fileName, callback) {
-        
-        object.loadData(fileName, function() {
-            
+    object.init = function (fileName, callback) {
+
+        object.loadData(fileName, function () {
+
             if (object.bodyState == 1) {
                 generateBody();
                 object.bodyState = 2;
@@ -76,15 +126,15 @@ var Skull = function(scale) {
     };
 
     function generateBody() {
-        
+
         var arr = object.bodyTxt.split("\n");
         var vertices = [];
         var faces = [];
 
-        arr.forEach(function(tupleTxt) {
-            
+        arr.forEach(function (tupleTxt) {
+
             var tuple = tupleTxt.split(" ");
-            
+
             if (tuple[0] == "v") {
                 for (var i = 1; i < 4; i++) tuple[i] = parseFloat(tuple[i]);
                 vertices.push(new THREE.Vector3(tuple[1], tuple[2] + object.scale, tuple[3]));
@@ -97,8 +147,6 @@ var Skull = function(scale) {
 
         });
 
-        console.log(vertices);
-
         var geometry = new THREE.Geometry();
         geometry.vertices = vertices;
         geometry.faces = faces;
@@ -108,39 +156,50 @@ var Skull = function(scale) {
         material.side = THREE.DoubleSide;
         material.color = new THREE.Color(0xddcccc);
 
-        object.bodyMesh = new THREE.Mesh( geometry, material );
+        object.bodyMesh = new THREE.Mesh(geometry, material);
 
     }
 
     function generateSticks() {
-        
-        var vertices = [
-            new THREE.Vector3(0, 0, 0),
-            new THREE.Vector3(0, 100, 0),
-            new THREE.Vector3(100, 0, 0),
-            new THREE.Vector3(0, 0, 100),
-        ];
 
-        var faces = [
-            new THREE.Face3(0, 1, 2),
-            new THREE.Face3(0, 1, 3),
-            new THREE.Face3(0, 2, 3),
-            new THREE.Face3(1, 2, 3),
-        ];
+        var arr = object.sticksTxt.split("\n");
+        object.sticksMesh = [];
+        object.sticks = [];
 
-        var geometry = new THREE.Geometry();
-        geometry.vertices = vertices;
-        geometry.faces = faces;
-        geometry.computeFaceNormals();
+        for (var i = 0; i < arr.length; i += 2) {
 
-        // var geometry = new THREE.BoxGeometry( 30, 10, 20 );
+            var tuple0 = arr[i].split(" ");
+            var tuple1 = arr[i + 1].split(" ");
 
-        var material = new THREE.MeshPhongMaterial();
-        material.side = THREE.DoubleSide;
-        material.color = new THREE.Color(0xaabb00);
-        
+            if (tuple0[0] == "Vertex" && tuple1[0] == "Vertex") {
 
-        object.sticksMesh = new THREE.Mesh( geometry, material );
+                for (var j = 2; j < 5; j++) tuple0[j] = parseFloat(tuple0[j]);
+                for (var j = 2; j < 5; j++) tuple1[j] = parseFloat(tuple1[j]);
+
+                var startPt = new THREE.Vector3(tuple0[2], tuple0[3], tuple0[4]);
+                var vec = new THREE.Vector3(tuple1[2] - tuple0[2], tuple1[3] - tuple0[3], tuple1[4] - tuple0[4]);
+                var len = vec.length();
+                vec.normalize();
+
+                var axis = getRotationAxis(vec);
+                var angle = getRotationAngle(vec);
+                console.log(axis, angle);
+                
+                var stick = {
+                    'startPt': startPt,
+                    'vec': vec,
+                    'len': len,
+                    'axis': axis,
+                    'angle': angle
+                };
+                object.sticks.push(stick);
+                
+                var stickMesh = getStickMesh(stick, object.scale);
+                object.sticksMesh.push(stickMesh);
+
+            }
+
+        }
 
     }
 
